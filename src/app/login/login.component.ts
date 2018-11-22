1c74c04599a33139a22921d80d5bd53548356622
davidxz <davidcardosleo@gmail.com>
2018-11-22 20:21:04 +0200

    login chapter 1

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  username:string;
+  pass:string;
+  list= new Array<String>();
+  contor:number;
+  msgUserG = 'Username Gresit';
   constructor() { }
-
+  
   ngOnInit() {
+    this.list = ["davidxz","roby","patrick","raul","mathi"];
+  }
+
+
+  chechUser(user) {
+   this.list.forEach(utilizator => {
+    if(utilizator === this.username) {
+       return true;
+     }
+     else{
+        return this.msgUserG;
+      }
+     
+   });
+    
   }
 
 }

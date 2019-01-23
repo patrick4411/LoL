317da14e622c8a748edd34b24d8e3521ca0898ce
rrosca <raul.rosca@hotmail.com>
2019-01-23 20:16:35 +0200

    html things

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import {ButtonModule} from 'primeng/button';
 import {InputTextModule} from 'primeng/inputtext';
 import {PasswordModule} from 'primeng/password';
+import {} from 'primeng/gr';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
@@ -19,7 +20,6 @@ import { ProfileComponent } from './profile/profile.component';
     ButtonModule,
     InputTextModule,
     PasswordModule,
-  
     FormsModule
   ],
   providers: [],
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {ButtonModule} from 'primeng/button';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
